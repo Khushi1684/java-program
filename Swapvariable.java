b6aaf83dd8d6c49136598ae3807c05c03be48ae1
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-10-23 16:27:48 +0530

    Update Swapvariable.java

--- Swapvariable.java
+++ Swapvariable.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;
+importjava.util.Scanner;
 public class Swapvariable {
     public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        int num1=sc.nextInt();
-        int num2=sc.nextInt();
-        
-
-
-
-
+      Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c,b,a;
+        c=b;
+        b=a;
+        a=c;
+        System.out.println("your swipe value is:: "+a,+b);
     }
 
     
