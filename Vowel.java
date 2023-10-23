6f39204db61530d3c3c861e7c26a5e3a2e2db96f
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-10-23 16:35:28 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -3,7 +3,14 @@ public class Vowel {
     public static void main(String[]args)
     {
         Scanner sc=new Scanner(System.in);
-        int v1=sc.nextInt();
+        System.out.println("Enter a Alphabet:")
+            int n=sc.nextInt();
+        
+        if(n==a||n==e||n==i||n==o||n==u)
+            System.out.println("it is vowel");
+        else
+            System.out.println("it is not a vowel");
+            
 
 
     }
