eb6fd2ce1eda89e049d92b7d1801c88cdc124bdd
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-10-23 16:20:02 +0530

    Update Hollow_triangle2.java

--- Hollow_triangle2.java
+++ Hollow_triangle2.java
@@ -1,3 +1,15 @@
 public class Hollow_triangle2 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER ROWS:");
+        int n =sc.nextInt();
+        for (int i=1;i<=n;i++)
+            {
+                for(int j=1;j,=i;j++){
+                    System.out.println("*");
+                }
+                System.out.println();
     
+            }
+    }
 }
