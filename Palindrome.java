ae57d5641da40fe65324942f50c91735f8c8c6b1
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-11-25 22:56:56 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(PairOfString[]args){
+      Scanner sc = new Scanner(System.in);
+      int number=sc.nextInt();
+           
+
+
+      }
+    }
+
+
