bb4e536ccdc64d705bf874b2cd3e4343d2d8ac2e
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-11-25 22:56:11 +0530

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,5 +1,5 @@
 public class pattern {
-    public static void main(String[]args){
+    public static void main(PairOfString[]args){
         for(int i=1;i<=5;i++)
         {
             for(int j=1;j<=5;j++){
