462cb0abeadfbceb86771eee29f0761649707062
Khushi1684 <132039068+Khushi1684@users.noreply.github.com>
2023-11-25 22:52:27 +0530

    Add files via upload

---
+++ insurance.java
@@ -0,0 +1,8 @@
+public class insurance {
+    public static void main(PairOfString[] args) {
+        System.out.println("person name");
+        System.out.println("person gender");
+        System.out.println("person age");
+        System.out.println("person city");
+    }
+}
